e4f84d545601b9f3e9f2936e8ec218d39b6c7783
rupendrachoudhary25 <rupendrachoudhary25@gmail.com>
2024-05-28 13:19:47 +0530

    Fully Functional To-Do List Using JavaScript

---
+++ script.js
@@ -0,0 +1,26 @@
+function addTask() {
+  let input = document.getElementById("inputText").value;
+  if (input === "") {
+    alert("You must write something!");
+    return;
+  }
+
+  let taskContainer = document.createElement("div");
+  taskContainer.classList.add("taskItem");
+
+  let taskContent = document.createElement("span");
+  taskContent.textContent = input;
+
+  let removeButton = document.createElement("button");
+  removeButton.innerHTML = '<i class="fas fa-trash"></i>';
+  removeButton.onclick = function () {
+    taskContainer.remove();
+  };
+
+  taskContainer.appendChild(taskContent);
+  taskContainer.appendChild(removeButton);
+
+  document.getElementById("toDoContainer").appendChild(taskContainer);
+
+  document.getElementById("inputText").value = "";
+}
